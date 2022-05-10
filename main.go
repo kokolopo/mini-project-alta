268533cb0d26b267e43a83667e00a0728523a2bc
Fahmi Hadi <Kokolopo321@gmail.com>
2022-05-10 12:59:14 +0700

    test

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"net/http"
 	"order_kafe/auth"
+	"order_kafe/category"
 	"order_kafe/config"
+	"order_kafe/controller"
 	"order_kafe/database"
 	"order_kafe/helper"
-	"order_kafe/order"
+	"order_kafe/item"
 	"order_kafe/user"
 	"strings"
 
@@ -19,50 +21,50 @@ func main() {
 	database.InitDatabase(conf)
 	db := database.DB
 
-	orderRepo := order.NewOrderRepository(db)
-	userRepo := user.NewUserRepository(db)
-	orderService := order.NewOrderService(orderRepo, userRepo)
-
-	var input order.InputNewOrder
-	input.UserID = 7
-	input.Infomation = "wait a payment"
-
-	orderService.CreateOrder(input)
-
+	// orderRepo := order.NewOrderRepository(db)
 	// userRepo := user.NewUserRepository(db)
-	// userService := user.NewUserService(userRepo)
-	// authService := auth.NewService()
-	// userController := controller.NewUserHandler(userService, authService)
+	// orderService := order.NewOrderService(orderRepo, userRepo)
 
-	// categoryRepo := category.NewCategoryRepository(db)
-	// categoryService := category.NewCategoryService(categoryRepo)
-	// categoryController := controller.NewCategoryHandler(categoryService)
+	// var input order.InputNewOrder
+	// input.UserID = 7
+	// input.Infomation = "wait a payment"
 
-	// itemRepo := item.NewItemRepository(db)
-	// itemService := item.NewItemService(itemRepo, categoryRepo)
-	// itemController := controller.NewItemHandler(itemService)
+	//orderService.CreateOrder(input)
 
-	// router := gin.Default()
-	// api := router.Group("/api/v1")
+	userRepo := user.NewUserRepository(db)
+	userService := user.NewUserService(userRepo)
+	authService := auth.NewService()
+	userController := controller.NewUserHandler(userService, authService)
 
-	// // user domain
-	// api.POST("/users", userController.UserRegister)
-	// api.POST("/sessions", userController.Login)
-	// api.POST("/email_checkers", userController.CheckEmailAvailability)
-	// api.PUT("/users/:id", userController.UpdateData)
-	// api.POST("/avatars", authMiddleware(authService, userService), userController.UploadAvatar)
+	categoryRepo := category.NewCategoryRepository(db)
+	categoryService := category.NewCategoryService(categoryRepo)
+	categoryController := controller.NewCategoryHandler(categoryService)
 
-	// // item domain
-	// api.POST("/items", authMiddleware(authService, userService), itemController.CreateNewItem)
-	// api.GET("/items", itemController.GetItems)
-	// api.PUT("/items/:id", itemController.UpdateItems)
-	// api.DELETE("/items/:id", itemController.DeleteItems)
+	itemRepo := item.NewItemRepository(db)
+	itemService := item.NewItemService(itemRepo, categoryRepo)
+	itemController := controller.NewItemHandler(itemService)
 
-	// // category domain
-	// api.POST("/categories", categoryController.CreateNewCategory)
-	// api.GET("/categories", categoryController.GetCategories)
+	router := gin.Default()
+	api := router.Group("/api/v1")
 
-	// router.Run(":8080")
+	// user domain
+	api.POST("/users", userController.UserRegister)
+	api.POST("/sessions", userController.Login)
+	api.POST("/email_checkers", userController.CheckEmailAvailability)
+	api.PUT("/users/:id", userController.UpdateData)
+	api.POST("/avatars", authMiddleware(authService, userService), userController.UploadAvatar)
+
+	// item domain
+	api.POST("/items", authMiddleware(authService, userService), itemController.CreateNewItem)
+	api.GET("/items", itemController.GetItems)
+	api.PUT("/items/:id", itemController.UpdateItems)
+	api.DELETE("/items/:id", itemController.DeleteItems)
+
+	// category domain
+	api.POST("/categories", categoryController.CreateNewCategory)
+	api.GET("/categories", categoryController.GetCategories)
+
+	router.Run(":8080")
 }
 
 func authMiddleware(authService auth.Service, userService user.UserService) gin.HandlerFunc {
